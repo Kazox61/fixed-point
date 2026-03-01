ecd7ba214388e0b8940ee7b0d01382c8839428b4
Daniil Pankevich <daniil.pankevich@gmail.com>
2026-03-01 19:10:25 +0300

    Add some helpful methods for structs

---
+++ Runtime/Fixed32/Structs/Vector3Utils.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Fixed32
+{
+	public static class Vector3Utils
+	{
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static FVector3 RotateAround(this FVector3 vector, FVector3 rotationOrigin, FQuaternion rotation)
+		{
+			return rotation * (vector - rotationOrigin) + rotationOrigin;
+		}
+		
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static FVector3 With(this FVector3 vector, FP? x = null, FP? y = null, FP? z = null)
+		{
+			return new FVector3
+			{
+				X = x ?? vector.X,
+				Y = y ?? vector.Y,
+				Z = z ?? vector.Z
+			};
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static FVector2 ToXZ(this FVector3 vector) => new FVector2(vector.X, vector.Z);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static FVector2 ToXY(this FVector3 vector) => new FVector2(vector.X, vector.Y);
+	}
+}
---
+++ Runtime/Fixed64/Structs/Vector2Utils.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Fixed64
+{
+	public static class Vector2Utils
+	{
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static FVector2 RotateAround(this FVector2 vector, FVector2 rotationOrigin, FRotation2D rotation2D)
+		{
+			return rotation2D * (vector - rotationOrigin) + rotationOrigin;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static FVector2 With(this FVector2 vector, FP? x = null, FP? y = null)
+		{
+			return new FVector2
+			{
+				X = x ?? vector.X,
+				Y = y ?? vector.Y
+			};
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static FVector3 FromXZ(this FVector2 vector, FP y = default) => new FVector3(vector.X, y, vector.Y);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static FVector3 FromXY(this FVector2 vector, FP z = default) => new FVector3(vector.X, vector.Y, z);
+	}
+}
